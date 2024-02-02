c953973d75514d029682a64b06adaca09fc40fd3
ChaseTeague <teaguechase@yahoo.com>
2024-02-02 16:17:04 -0500

    complete lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Chase</h1>
+      <img src="https://via.placeholder.com/350" alt="img of self"/>
+      <h2>About Me</h2>
+      <p>bio paragraph</p>
+      <a href='https://github.com'>GitHub</a>
+      <a href='https://linkedin.com'>LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,54 @@ import '@testing-library/jest-dom';
 import App from "../App";
 
 // Your tests here
+test("displays a top-leveling heading with text `Hi, I'm ______`", () => {
+    render(<App />);
+    const topLevelHeading = screen.getByRole("heading", {
+        name: /hi, i'm/i,
+        exact: false,
+        level: 1
+    });
+    expect(topLevelHeading).toBeInTheDocument();
+})
+
+test("displays an image of yourself wit alt text identifying the image", () => {
+    render(<App />);
+    const imgOfYourself = screen.getByAltText("img of self");
+    expect(imgOfYourself).toHaveAttribute('src', "https://via.placeholder.com/350")
+})
+
+test("displays second level heading with text content of 'About Me'", () => {
+    render(<App />)
+    const secondHeading = screen.getByRole('heading', {
+        name : /about me/i,
+        level: 2
+    })
+    expect(secondHeading).toBeInTheDocument();
+})
+
+test("displays a paragraph", () => {
+    render(<App />)
+    const paragraph = screen.getByText(/bio paragraph/i);
+    expect(paragraph).toBeInTheDocument();
+})
+
+test("displays the correct links", () => {
+    render(<App />);
+  
+    const githubLink = screen.getByRole("link", {
+      name: /github/i,
+    });
+    const linkedinLink = screen.getByRole("link", {
+      name: /linkedin/i,
+    });
+  
+    expect(githubLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://github.com")
+    );
+  
+    expect(linkedinLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("https://linkedin.com")
+    );
+  });
